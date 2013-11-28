68ea4aa629542d977ad95db6376a1f001d6788e1
Kirill Klenski <kirill@codeborne.com>
2013-11-28 10:23:19 +0200

    uses relative path instead of absolute

--- indicator.sh
+++ indicator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ps aux | fgrep python | fgrep indicator.py | fgrep -v grep > /dev/null
 if [ "$?" != "0" ] ; then
-  python /home/xp/work/dev-indicator/indicator.py > /home/xp/work/dev-indicator/indicator.log 2>&1 &
+  python `dirname $0`/indicator.py > `dirname $0`/indicator.log 2>&1 &
 fi
 
 ps aux | fgrep python | fgrep jenkins_desktop_notify.py | fgrep -v grep > /dev/null
