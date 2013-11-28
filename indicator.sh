3858c8e03ad8935bc7937801061a340ebe72be32
Marko Randrüüt <marko@codeborne.com>
2013-11-28 12:08:38 +0200

    uses current directory instead of hardcoded absolute path

--- indicator.sh
+++ indicator.sh
@@ -6,5 +6,5 @@ fi
 
 ps aux | fgrep python | fgrep jenkins_desktop_notify.py | fgrep -v grep > /dev/null
 if [ "$?" != "0" ] ; then
-  python /home/xp/work/dev-indicator/jenkins_desktop_notify.py > /home/xp/work/dev-indicator/jenkins_desktop_notify.log 2>&1 &
+  python `dirname $0`/jenkins_desktop_notify.py > `dirname $0`/jenkins_desktop_notify.log 2>&1 &
 fi
