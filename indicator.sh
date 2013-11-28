22a3618948f3da52cc5c22ba84bfdc3c4b797d0a
Andrei Solntsev <andrei@codeborne.com>
2013-11-28 09:49:16 +0200

    run jenkins checker from sh file too

--- indicator.sh
+++ indicator.sh
@@ -3,3 +3,8 @@ ps aux | fgrep python | fgrep indicator.py | fgrep -v grep > /dev/null
 if [ "$?" != "0" ] ; then
   python /home/xp/work/dev-indicator/indicator.py > /home/xp/work/dev-indicator/indicator.log 2>&1 &
 fi
+
+ps aux | fgrep python | fgrep jenkins_desktop_notify.py | fgrep -v grep > /dev/null
+if [ "$?" != "0" ] ; then
+  python /home/xp/work/dev-indicator/jenkins_desktop_notify.py > /home/xp/work/dev-indicator/jenkins_desktop_notify.log 2>&1 &
+fi
