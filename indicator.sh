92d82ce2f4d462d362a7f784c8cf0dbe0df3d743
Anton Keks <anton@azib.net>
2014-10-09 10:43:50 +0300

    apt-get comment added

--- indicator.sh
+++ indicator.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# sudo apt-get install python-appindicator
 ps aux | fgrep python | fgrep indicator.py | fgrep -v grep > /dev/null
 if [ "$?" != "0" ] ; then
   python `dirname $0`/indicator.py > `dirname $0`/indicator.log 2>&1 &
